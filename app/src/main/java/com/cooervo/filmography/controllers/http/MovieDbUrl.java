92e9c1dffb45f15077db2b0dd3a1d3e82ce940e1
cooervoAtDesktop <cuervo.taylor@gmail.com>
2015-08-06 15:56:13 -0500

    added singleton comments

--- app/src/main/java/com/cooervo/filmography/controllers/http/MovieDbUrl.java
+++ app/src/main/java/com/cooervo/filmography/controllers/http/MovieDbUrl.java
@@ -5,22 +5,21 @@ package com.cooervo.filmography.controllers.http;
  */
 public class MovieDbUrl {
 
+     //Volatile keyword ensures that multiple threads handle the unique/instance correctly
     private volatile static MovieDbUrl uniqueInstance;
 
     private final String url = "http://api.themoviedb.org/3/";
     private final String API_KEY = "deea9711e0770caae3fc592b028bb17e";
-    private String actorQuery;
-    private String filmographyQuery;
-
 
     private MovieDbUrl() {
     }
 
+    //Check for an instance and if there isn't one enter the synchronized method
     public static MovieDbUrl getInstance() {
         if (uniqueInstance == null) {
             synchronized (MovieDbUrl.class) {
                 if (uniqueInstance == null) {
-                    uniqueInstance = new MovieDbUrl();
+                    uniqueInstance = new MovieDbUrl(); //Once in the block, check again and if still null, create the instance
                 }
             }
         }
